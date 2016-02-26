fb8510dccf0d39343388317828317294f60c51dc
diitaz93 <juantrix93@hotmail.com>
2016-02-26 17:50:19 -0500

    solo python y make (falta)

---
+++ tarea_1/3_cuerpos/fig16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main ()
+{
+  long seed;
+  int n=400;
+  int i;
+  seed=n;
+  srand48( seed );
+  for (i=0;i<n;i++)
+    {
+      printf("%.4f %.4f\n",drand48(),drand48());
+    }
+  return 1;
+}
--- tarea_1/3_cuerpos/figuras.py
+++ tarea_1/3_cuerpos/figuras.py
@@ -19,18 +19,60 @@ plt.xlabel(r'$q_3$')
 plt.title(r'Figura 10')
 
 #FIGURA 11
+data=np.genfromtxt('fig11.dat')
+fig11=plt.figure(figsize=(10,10))
+plt.scatter(data[:,0],data[:,1],s=1,c='k')
+plt.ylabel(r'$p_3$')
+plt.xlabel(r'$q_3$')
+plt.title(r'Figura 11')
 
 #FIGURA 12
+data=np.genfromtxt('fig12.dat')
+fig12=plt.figure(figsize=(10,10))
+plt.scatter(data[:,0],data[:,1],s=1,c='k')
+plt.ylabel(r'$p_3$')
+plt.xlabel(r'$q_3$')
+plt.title(r'Figura 12')
 
 #FIGURA 13
+data=np.genfromtxt('fig13.dat')
+fig13=plt.figure(figsize=(10,10))
+plt.scatter(data[:,0],data[:,1],s=1,c='k')
+plt.ylabel(r'$p_3$')
+plt.xlabel(r'$q_3$')
+plt.title(r'Figura 13')
 
 #FIGURA 14
+data=np.genfromtxt('fig14.dat')
+fig14=plt.figure(figsize=(10,10))
+plt.scatter(data[:,0],data[:,1],s=1,c='k')
+plt.ylabel(r'$p_3$')
+plt.xlabel(r'$q_3$')
+plt.title(r'Figura 14')
 
 #FIGURA 15
+data=np.genfromtxt('fig15.dat')
+fig15=plt.figure(figsize=(10,10))
+plt.scatter(data[:,0],data[:,1],s=1,c='k')
+plt.ylabel(r'$p_3$')
+plt.xlabel(r'$q_3$')
+plt.title(r'Figura 15')
 
 #FIGURA 16
+data=np.genfromtxt('fig16.dat')
+fig16=plt.figure(figsize=(10,10))
+plt.scatter(data[:,0],data[:,1],s=1,c='k')
+plt.ylabel(r'$p_3$')
+plt.xlabel(r'$q_3$')
+plt.title(r'Figura 16')
 
 #FIGURA 17
+data=np.genfromtxt('fig17.dat')
+fig17=plt.figure(figsize=(10,10))
+plt.scatter(data[:,0],data[:,1],s=1,c='k')
+plt.ylabel(r'$p_3$')
+plt.xlabel(r'$q_3$')
+plt.title(r'Figura 17')
 
 #Guarda imagenes en un pdf
 pdf=PdfPages('Figuras.pdf')
